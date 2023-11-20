ce139f4aa737a8885e9f99a0940709b83e69617d
George Atkins <g.o.atkins@gmail.com>
2023-11-20 15:03:22 +1300

    Added original code for reference

---
+++ Original Example.py
@@ -0,0 +1,308 @@
+"""
+windows
+=======
+
+python - 3.11.4
+
+Need to download and install VLC - https://www.videolan.org/
+
+python extensions:
+pip install bleak
+pip install python-vlc
+
+hub to pc comms example - https://pybricks.com/projects/tutorials/wireless/hub-to-device/pc-communication/
+video player example - https://www.makeuseof.com/pygame-sprite-game-characters-create/
+-> simpler example here (but not tested): https://github.com/PaulleDemon/tkVideoPlayer/issues/2 
+async tkinter helper - https://pypi.org/project/async-tkinter-loop/
+
+"""
+
+
+print("Program Start")
+
+import tkinter as tk
+import vlc
+from tkinter import filedialog
+from datetime import timedelta
+
+import asyncio
+from bleak import BleakScanner, BleakClient
+
+from async_tkinter_loop import async_handler, async_mainloop
+
+UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+# Replace this with the name of your hub if you changed
+# it when installing the Pybricks firmware.
+HUB_NAME = "Little Dawg"
+
+
+class MediaPlayerApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Media Player")
+        self.geometry("800x600")
+        self.configure(bg="#f0f0f0")
+        self.initialize_player()        
+    
+    def initialize_player(self):
+        self.instance = vlc.Instance()
+        self.media_player = self.instance.media_player_new()
+        self.current_file = None
+        self.playing_video = False
+        self.video_paused = False
+        self.create_widgets()     
+
+    def fll_play_video(self):
+        self.stop()
+        self.current_file = "C:/FLL/20220515_104244.mp4"
+        self.time_label.config(text="00:00:00 / " + self.get_duration_str())
+        self.play_video()
+
+    def create_widgets(self):
+        self.media_canvas = tk.Canvas(self, bg="black", width=800, height=400)
+        self.media_canvas.pack(pady=10, fill=tk.BOTH, expand=True)
+        self.select_file_button = tk.Button(
+            self,
+            text="Select File",
+            font=("Arial", 12, "bold"),
+            command=self.select_file,
+        )
+        self.select_file_button.pack(pady=5)
+        self.time_label = tk.Label(
+            self,
+            text="00:00:00 / 00:00:00",
+            font=("Arial", 12, "bold"),
+            fg="#555555",
+            bg="#f0f0f0",
+        )
+        self.time_label.pack(pady=5)
+        self.control_buttons_frame = tk.Frame(self, bg="#f0f0f0")
+        self.control_buttons_frame.pack(pady=5)
+    
+        self.play_button = tk.Button(
+            self.control_buttons_frame,
+            text="Play",
+            font=("Arial", 12, "bold"),
+            bg="#4CAF50",
+            fg="white",
+            command=self.play_video,
+        )
+        self.play_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.pause_button = tk.Button(
+            self.control_buttons_frame,
+            text="Pause",
+            font=("Arial", 12, "bold"),
+            bg="#FF9800",
+            fg="white",
+            command=self.pause_video,
+        )
+        self.pause_button.pack(side=tk.LEFT, padx=10, pady=5)
+        self.stop_button = tk.Button(
+            self.control_buttons_frame,
+            text="Stop",
+            font=("Arial", 12, "bold"),
+            bg="#F44336",
+            fg="white",
+            command=self.stop,
+        )
+        self.stop_button.pack(side=tk.LEFT, pady=5)
+        self.fast_forward_button = tk.Button(
+            self.control_buttons_frame,
+            text="Fast Forward",
+            font=("Arial", 12, "bold"),
+            bg="#2196F3",
+            fg="white",
+            command=self.fast_forward,
+        )
+        self.fast_forward_button.pack(side=tk.LEFT, padx=10, pady=5)
+        self.rewind_button = tk.Button(
+            self.control_buttons_frame,
+            text="Rewind",
+            font=("Arial", 12, "bold"),
+            bg="#2196F3",
+            fg="white",
+            command=self.rewind,
+        )
+        self.rewind_button.pack(side=tk.LEFT, pady=5)
+        self.progress_bar = VideoProgressBar(
+            self, self.set_video_position, bg="#e0e0e0", highlightthickness=0
+        )
+        self.progress_bar.pack(fill=tk.X, padx=10, pady=5)
+
+    def select_file(self):
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Media Files", "*.mp4 *.avi .mov")]
+        )
+        if file_path:
+            self.current_file = file_path
+            self.time_label.config(text="00:00:00 / " + self.get_duration_str())
+            self.play_video()
+
+    def get_duration_str(self):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            total_duration_str = str(timedelta(milliseconds=total_duration))[:-3]
+            return total_duration_str
+        return "00:00:00"
+    
+    def play_video(self):
+        if not self.playing_video:
+            media = self.instance.media_new(self.current_file)
+            self.media_player.set_media(media)
+            self.media_player.set_hwnd(self.media_canvas.winfo_id())
+            self.media_player.play()
+            self.playing_video = True
+
+    def fast_forward(self):
+        if self.playing_video:
+            current_time = self.media_player.get_time() + 10000
+            self.media_player.set_time(current_time)
+
+    def rewind(self):
+        if self.playing_video:
+            current_time = self.media_player.get_time() - 10000
+            self.media_player.set_time(current_time)
+
+    def pause_video(self):
+        if self.playing_video:
+            if self.video_paused:
+                self.media_player.play()
+                self.video_paused = False
+                self.pause_button.config(text="Pause")
+            else:
+                self.media_player.pause()
+                self.video_paused = True
+                self.pause_button.config(text="Resume")
+
+    def stop(self):
+        if self.playing_video:
+            self.media_player.stop()
+            self.playing_video = False
+        self.time_label.config(text="00:00:00 / " + self.get_duration_str())
+
+    def set_video_position(self, value):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            position = int((float(value) / 100) * total_duration)
+            self.media_player.set_time(position)
+
+    def update_video_progress(self):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            current_time = self.media_player.get_time()
+            progress_percentage = (current_time / total_duration) * 100
+            self.progress_bar.set(progress_percentage)
+            current_time_str = str(timedelta(milliseconds=current_time))[:-3]
+            total_duration_str = str(timedelta(milliseconds=total_duration))[:-3]
+            self.time_label.config(text=f"{current_time_str}/{total_duration_str}")
+        self.after(1000, self.update_video_progress)
+
+class VideoProgressBar(tk.Scale):
+    def __init__(self, master, command, **kwargs):
+        kwargs["showvalue"] = False
+        super().__init__(
+            master,
+            from_=0,
+            to=100,
+            orient=tk.HORIZONTAL,
+            length=800,
+            command=command,
+            **kwargs,
+        )
+        self.bind("<Button-1>", self.on_click)
+
+    def on_click(self, event):
+        if self.cget("state") == tk.NORMAL:
+            value = (event.x / self.winfo_width()) * 100
+            self.set(value)
+
+print("Running....")
+
+# Run the main async program.
+#asyncio.run(main())
+
+def greet_user():
+    name = input('Enter your name ')
+    print('Good Morning ',name)
+
+
+def hub_filter(device, ad):
+    print(device.name)
+    return device.name and device.name.lower() == HUB_NAME.lower()
+
+
+def handle_disconnect(_):
+    print("Hub was disconnected.")
+
+
+def handle_rx(_, data: bytearray):
+    print("Received:", data)
+
+    pos = data.find(b"Play Video")
+    if pos >= 0:
+        app.fll_play_video()
+
+@async_handler
+async def main(app):
+    # Find the device and initialize client.
+    device = await BleakScanner.find_device_by_filter(hub_filter)
+    client = BleakClient(device, disconnected_callback=handle_disconnect)
+
+    # Shorthand for sending some data to the hub.
+    async def send(client, data):
+        await client.write_gatt_char(rx_char, data)
+
+    try:
+        # Connect and get services.
+        await client.connect()
+        await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
+        nus = client.services.get_service(UART_SERVICE_UUID)
+        rx_char = nus.get_characteristic(UART_RX_CHAR_UUID)
+
+        # Tell user to start program on the hub.
+        print("Start the program on the hub now with the button.")
+
+        # Send a few messages to the hub.
+        for i in range(5):
+            await send(client, b"fwd")
+            await asyncio.sleep(1)
+            await send(client, b"rev")
+            await asyncio.sleep(1)
+
+        #app.fll_play_video()
+
+        # Send a message to indicate stop.
+        await send(client, b"bye")
+        
+
+    except Exception as e:
+        # Handle exceptions.
+        print(e)
+    #finally:
+        # Disconnect when we are done.
+        #await client.disconnect()
+
+app = MediaPlayerApp()
+
+if __name__ == "__main__":
+    print("h0")
+    #asyncio.run(app.listenForHub())
+    #app.update_video_progress()
+    #app.fll_play_video()
+    print("h1")
+    #app.after(2000, asyncio.run(main()))
+    #app.after(0, greet_user)
+    app.after(2000, async_handler(main(app)))
+    async_mainloop(app)
+    
+
+    #app.mainloop()
+    print("h2")
+
+
+
+msg = "Roll a dice"
+print(msg)
